a670a0d6868b26b1e0604ed0e5e7c8f7de30c432
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2025-02-05 16:43:39 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,116 @@
+import asyncio
+from threading import Event
+from typing import ClassVar, Final, List, Mapping, Optional, Sequence
+
+from typing_extensions import Self
+from viam.components.generic import *
+from viam.module.module import Module
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import Geometry, ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.easy_resource import EasyResource
+from viam.resource.types import Model, ModelFamily
+from viam.utils import ValueTypes
+
+
+LOGGER = getLogger("control-logic")
+
+class ControlLogic(Generic, EasyResource):
+    MODEL: ClassVar[Model] = Model(
+        ModelFamily("naomi", "my-control-logic"), "control-logic"
+    )
+    task = None
+    event = Event()
+
+    @classmethod
+    def new(
+        cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
+        """This method creates a new instance of this Generic component.
+        The default implementation sets the name from the `config` parameter and then calls `reconfigure`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+            dependencies (Mapping[ResourceName, ResourceBase]): The dependencies (both implicit and explicit)
+
+        Returns:
+            Self: The resource
+        """
+        return super().new(config, dependencies)
+
+    @classmethod
+    def validate_config(cls, config: ComponentConfig) -> Sequence[str]:
+        """This method allows you to validate the configuration object received from the machine,
+        as well as to return any implicit dependencies based on that `config`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+
+        Returns:
+            Sequence[str]: A list of implicit dependencies
+        """
+        return []
+
+    def reconfigure(
+        self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ):
+        """This method allows you to dynamically update your service when it receives a new `config` object.
+
+        Args:
+            config (ComponentConfig): The new configuration
+            dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
+        """
+        if self.running is not None:
+            self.start()
+        else:
+            LOGGER.info("Already running control logic.")
+
+    def start(self):
+        loop = asyncio.get_event_loop()
+        self.task = loop.create_task(self.control_loop())
+        self.event.clear()
+
+    def stop(self):
+        self.event.set()
+        if self.task is not None:
+            self.task.cancel()
+
+    async def control_loop(self):
+        while not self.event.is_set():
+            await self.on_loop()
+            await asyncio.sleep(0)
+
+    async def on_loop(self):
+        try:
+            LOGGER.info("Executing control logic")
+            # TODO: ADD CONTROL LOGIC
+
+        except Exception as err:
+            LOGGER.error(err)
+
+    def __del__(self):
+        self.stop()
+
+    async def close(self):
+        self.stop()
+
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Mapping[str, ValueTypes]:
+        result = {key: False for key in command.keys()}
+        for name, _args in command.items():
+            if name == "start":
+                self.start()
+                result[name] = True
+            if name == "stop":
+                self.stop()
+                result[name] = True
+        return result
+
+
+if __name__ == "__main__":
+    asyncio.run(Module.run_from_registry())
